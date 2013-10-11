788af396483b49a60e0e1101f0228704565c7b97
Anil <anil@cavvia.net>
2013-10-11 11:48:57 +0100

    clean up connection handling.

--- lib/set.rb
+++ lib/set.rb
@@ -3,7 +3,7 @@ module Forgetsy
   # A data structure that decays counts exponentially
   # over time. Decay is applied at read time.
   class Set
-    attr_accessor :name, :_conn
+    attr_accessor :name, :conn
 
     @@last_decayed_key = '_last_decay'
     @@lifetime_key = '_t'
@@ -20,6 +20,7 @@ module Forgetsy
         raise ArgumentError, "Please specify a mean lifetime using the 't' option"
       end
 
+      setup_conn
       date = opts[:date] || Time.now
       update_decay_date(date)
       conn.zadd(name, opts[:t].to_f, @@lifetime_key)
@@ -28,10 +29,10 @@ module Forgetsy
     # Fetch the entire set, or optionally ask for
     # the top :n results, or an individual :bin.
     #
-    # @example Retrive top n results
+    # @example Retrieve top n results
     #   set.fetch(n: 20)
     #
-    # @example Retrive an individual bin
+    # @example Retrieve an individual bin
     #   set.fetch(bin: 'foo')
     #
     # @return Hash
@@ -40,7 +41,7 @@ module Forgetsy
       decay if opts.fetch(:decay, true)
 
       if opts.key?(:bin)
-        result = [opts[:bin], conn.zscore(@name, opts[:bin])]
+        result = [opts[:bin], @conn.zscore(@name, opts[:bin])]
       else
         result = fetch_raw(limit: limit)
       end
@@ -56,29 +57,29 @@ module Forgetsy
       delta_t = t1 - t0
       set = fetch_raw
       rate = 1 / Float(lifetime)
-      conn.pipelined do
+      @conn.pipelined do
         set.each do |k, v|
           new_v = v * Math.exp(- delta_t * rate)
-          conn.zadd(@name, new_v, k)
+          @conn.zadd(@name, new_v, k)
         end
         update_decay_date(Time.now)
       end
     end
 
     def incr(bin)
-      conn.zincrby(@name, 1, bin)
+      @conn.zincrby(@name, 1, bin)
     end
 
     def incr_by(bin, by)
-      conn.zincrby(@name, by, bin)
+      @conn.zincrby(@name, by, bin)
     end
 
     def last_decayed_date
-      conn.zscore(@name, @@last_decayed_key)
+      @conn.zscore(@name, @@last_decayed_key)
     end
 
     def lifetime
-      conn.zscore(@name, @@lifetime_key)
+      @conn.zscore(@name, @@lifetime_key)
     end
 
     private
@@ -93,16 +94,16 @@ module Forgetsy
         buffered_limit = limit + special_keys.length
       end
 
-      set = conn.zrevrange(@name, 0, buffered_limit, withscores: true)
+      set = @conn.zrevrange(@name, 0, buffered_limit, withscores: true)
       filter_special_keys(set)[0..limit]
     end
 
     def update_decay_date(date)
-      conn.zadd(@name, date.to_f, @@last_decayed_key)
+      @conn.zadd(@name, date.to_f, @@last_decayed_key)
     end
 
-    def conn
-      @_conn ||= Forgetsy::Connection.fetch
+    def setup_conn
+      @conn ||= Forgetsy::Connection.fetch
     end
 
     def special_keys
