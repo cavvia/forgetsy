69307f6e0488352be1502d7f17cb89afdd6a28ca
Anil <anil@cavvia.net>
2013-10-13 17:27:47 +0100

    fix requires

--- lib/forgetsy.rb
+++ lib/forgetsy.rb
@@ -1,3 +1,3 @@
-require File.expand_path("../conn.rb", __FILE__)
-require File.expand_path("../set.rb", __FILE__)
-require File.expand_path("../delta.rb", __FILE__)
+require 'conn'
+require 'set'
+require 'delta'
